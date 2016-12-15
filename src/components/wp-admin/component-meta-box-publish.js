6b40918eb61c2196fbc83c7df8ef24701bce3670
Akeda Bagus <admin@gedex.web.id>
2016-12-15 22:03:24 +0700

    Added move to trash action in publish meta box component.

--- src/components/wp-admin/component-meta-box-publish.js
+++ src/components/wp-admin/component-meta-box-publish.js
@@ -11,6 +11,7 @@ import ComponentMetaBox from './component-meta-box';
 
 const METABOX_SELECTOR = By.css( '#submitdiv' );
 const PUBLISH_SELECTOR = By.css( '#publish' );
+const MOVE_TO_TRASH_SELECTOR = By.css( '.submitdelete' );
 
 export default class ComponentMetaBoxPublish extends ComponentMetaBox {
 	constructor( driver ) {
@@ -23,5 +24,13 @@ export default class ComponentMetaBoxPublish extends ComponentMetaBox {
 			perform();
 
 		return helper.clickWhenClickable( this.driver, PUBLISH_SELECTOR );
+	}
+
+	moveToTrash() {
+		this.driver.actions().
+			mouseMove( this.driver.findElement( MOVE_TO_TRASH_SELECTOR ) ).
+			perform();
+
+		return helper.clickWhenClickable( this.driver, MOVE_TO_TRASH_SELECTOR );
 	}
 }
