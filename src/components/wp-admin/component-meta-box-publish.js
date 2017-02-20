841c6b8f058928ab68ec0aabf33358d0875aab9d
Akeda Bagus <admin@gedex.web.id>
2017-02-20 22:56:33 +0700

    Added save, selectStatus, and hasStatus to publish meta box component.
    See #2.

--- src/components/wp-admin/component-meta-box-publish.js
+++ src/components/wp-admin/component-meta-box-publish.js
@@ -10,7 +10,11 @@ import { WebDriverHelper as helper } from 'wp-e2e-webdriver';
 import ComponentMetaBox from './component-meta-box';
 
 const METABOX_SELECTOR = By.css( '#submitdiv' );
+const SAVE_SELECTOR = By.css( '#save-post' );
 const PUBLISH_SELECTOR = By.css( '#publish' );
+const EDIT_STATUS_SELECTOR = By.css( '.edit-post-status' );
+const STATUS_SELECTOR = By.css( '#post_status' );
+const SAVE_STATUS_SELECTOR = By.css( '.save-post-status' );
 const MOVE_TO_TRASH_SELECTOR = By.css( '.submitdelete' );
 
 export default class ComponentMetaBoxPublish extends ComponentMetaBox {
@@ -19,18 +23,29 @@ export default class ComponentMetaBoxPublish extends ComponentMetaBox {
 	}
 
 	publish() {
-		this.driver.actions().
-			mouseMove( this.driver.findElement( PUBLISH_SELECTOR ) ).
-			perform();
-
+		helper.mouseMoveTo( this.driver, PUBLISH_SELECTOR );
 		return helper.clickWhenClickable( this.driver, PUBLISH_SELECTOR );
 	}
 
-	moveToTrash() {
-		this.driver.actions().
-			mouseMove( this.driver.findElement( MOVE_TO_TRASH_SELECTOR ) ).
-			perform();
+	save() {
+		helper.mouseMoveTo( this.driver, SAVE_SELECTOR );
+		return helper.clickWhenClickable( this.driver, SAVE_SELECTOR );
+	}
 
+	selectStatus( status ) {
+		helper.mouseMoveTo( this.driver, EDIT_STATUS_SELECTOR );
+		helper.clickWhenClickable( this.driver, EDIT_STATUS_SELECTOR );
+		helper.selectOption( this.driver, STATUS_SELECTOR, status );
+		return helper.clickWhenClickable( this.driver, SAVE_STATUS_SELECTOR );
+	}
+
+	hasStatus( status ) {
+		const selector = By.xpath( `//span[@id="post-status-display" and contains(text(), "${ status }")]` );
+		return helper.isEventuallyPresentAndDisplayed( this.driver, selector );
+	}
+
+	moveToTrash() {
+		helper.mouseMoveTo( this.driver, MOVE_TO_TRASH_SELECTOR );
 		return helper.clickWhenClickable( this.driver, MOVE_TO_TRASH_SELECTOR );
 	}
 }
